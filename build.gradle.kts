b9c4a30f9928ab78ea309d1dee14872504c6b613
Владислав <vladislavsumin@mail.ru>
2024-10-15 18:44:37 +0300

    add some shared scripts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,30 +2,14 @@ plugins {
     `maven-publish`
 }
 
+apply { from("build-scripts/group.gradle.kts") }
+apply { from("build-scripts/version.gradle.kts") }
+
 allprojects {
-    setupGroup()
-    version = "0.0.1"
     setupMavenPublish()
 }
 
-/**
- * Настраивает [Project.getGroup] для [Project] на основе пути по которому он расположен.
- *
- * Это необходимо для корректной публикации на maven репозитории, а так же для верного (и общего с maven) определения
- * артефактов, см vs-components.gradle.kts
- */
-fun Project.setupGroup() {
-    val path = mutableListOf<String>()
-    var project = this.parent
-    while (project != null && project != project.rootProject) {
-        path += project.name
-        project = project.parent
-    }
-    val subpackage = path.reversed().joinToString(separator = ".")
 
-    group = if (subpackage.isBlank()) "ru.vs"
-    else "ru.vs.$subpackage"
-}
 
 /**
  * Настраивает публикацию в maven репозиторий для [Project].
